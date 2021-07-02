4a395ccbadce5bf11ea042912f800d0871f10803
Jayson Cleary <jayson.cleary@gmail.com>
2021-07-02 07:51:13 -0400

    Fix model loader path

--- server/src/db/models/index.ts
+++ server/src/db/models/index.ts
@@ -1,7 +1,7 @@
 import * as sequelize from 'sequelize';
 import { swatchFactory } from './swatch';
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require('../config.json')[env];
 
 export const dbConfig = new sequelize.Sequelize(config.database, config.username, config.password, config);
 
